35fdba5addd7655a81c220af12fb5cad41b96b90
chrisstopher <chrisstophervan@gmail.com>
2013-11-11 22:03:24 -0500

    Took off bold metal font

--- src/texteditor/Main.java
+++ src/texteditor/Main.java
@@ -1,9 +1,11 @@
 package texteditor;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Main {
     public static void main(String[] args) {
+    	UIManager.put("swing.boldMetal", Boolean.FALSE);
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new GUI();
